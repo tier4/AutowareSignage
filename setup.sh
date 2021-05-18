7e1a200cb1f59691c9ca173d87316e4f3d5c6e4c
yabuta <tefutefu1989@gmail.com>
2021-05-19 08:38:55 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # aptで必要なパッケージをインストール
 sudo apt update -y
-sudo apt -y install qml-module-qtmultimedia qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtgraphicaleffects qml-module-qtquick-dialogs libqt5multimedia5-plugins qml-module-qtquick-shapes
+sudo apt -y install python3-pyqt5.qtquick qml-module-qtquick-controls2 qml-module-qtquick-shapes
 
 # signageのbuild
 colcon build
