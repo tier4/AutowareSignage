f7f1a7db7f8948210106dfb6def3ce6f66f7a5af
khtan <khtan>
2021-06-07 15:03:28 +0900

    change setup sh for ros1 support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # aptで必要なパッケージをインストール
 sudo apt update -y
-sudo apt -y install python3-pyqt5.qtquick qml-module-qtquick-controls2 qml-module-qtquick-shapes
+sudo apt -y install python-pyqt5.qtquick qml-module-qtquick-controls2 qml-module-qtquick-controls
 
 # signageのbuild
 colcon build
