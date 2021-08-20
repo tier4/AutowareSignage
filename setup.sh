6853b214d64b2322a87a27a9c2c495865b680865
tkhmy <tkh.my.p@gmail.com>
2021-08-20 11:00:12 +0900

    add required library
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # aptで必要なパッケージをインストール
 sudo apt update -y
-sudo apt -y install python3-pyqt5.qtquick qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-shapes python3-pyqt5.qtmultimedia
+sudo apt -y install python3-pyqt5.qtquick qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-shapes python3-pyqt5.qtmultimedia libasound2-dev
 sudo pip3 install simpleaudio
 
 # signageのbuild
