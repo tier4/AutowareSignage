8c9606b0e67726c4505ec04e0b46e38d92d81537
tkhmy <tkh.my.p@gmail.com>
2021-10-01 12:46:18 +0900

    commend out the door open and close
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- src/signage/src/signage/announce_controller.py
+++ src/signage/src/signage/announce_controller.py
@@ -39,7 +39,7 @@ class AnnounceControllerProperty():
         self._is_auto_mode = False
         self._is_auto_running = False
         self._door_announce = True
-        self._pre_door_announce_status = 0
+        # self._pre_door_announce_status = 0
         self._pending_announce_list = []
         self._emergency_trigger_time = 0
         self._sound = QSound("")
@@ -163,13 +163,13 @@ class AnnounceControllerProperty():
             # Only announce when the bus reach the goal and not in emergency state
             self.send_announce("thank_you")
             self._door_announce = True
-        elif door_status == 3 and self._pre_door_announce_status != 3:
-            # Should able to give warning everytime the door is opening
-            self.send_announce("door_open")
-            self._pre_door_announce_status = door_status
-        elif door_status == 4 and self._pre_door_announce_status != 4:
-            # Should able to give warning everytime the door is closing
-            self.send_announce("door_close")
-            self._pre_door_announce_status = door_status
-        elif door_status in [0, 2, 5]:
-            self._pre_door_announce_status = 0
+        # elif door_status == 3 and self._pre_door_announce_status != 3:
+        #     # Should able to give warning everytime the door is opening
+        #     self.send_announce("door_open")
+        #     self._pre_door_announce_status = door_status
+        # elif door_status == 4 and self._pre_door_announce_status != 4:
+        #     # Should able to give warning everytime the door is closing
+        #     self.send_announce("door_close")
+        #     self._pre_door_announce_status = door_status
+        # elif door_status in [0, 2, 5]:
+        #     self._pre_door_announce_status = 0
