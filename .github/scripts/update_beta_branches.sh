1c77a01a1adce7893e6f453365a8e06bfaee9fb2
khtan <tkh.my.p@gmail.com>
2022-10-13 14:25:10 +0900

    add beta repos, and fix the script
    Signed-off-by: khtan <tkh.my.p@gmail.com>

--- .github/scripts/update_beta_branches.sh
+++ .github/scripts/update_beta_branches.sh
@@ -35,4 +35,4 @@ git diff --cached --exit-code || git commit -m "Update repos files"
 
 echo -e "\e[36mComplete.\e[m"
 
-rm -rf autoware-release-scripts/ src/tier4_autoware_msgs src/auto_msgs
+rm -rf autoware-release-scripts/ src/*_msgs
