9b17d18eca61d6cf9a51c47f2e6138f72db84967
Rin Iwai <rin.iwai@tier4.jp>
2023-09-29 15:48:43 +0900

    fix updating system volume

--- src/signage/src/signage/announce_controller.py
+++ src/signage/src/signage/announce_controller.py
@@ -41,8 +41,6 @@ class AnnounceControllerProperty:
         self._check_playing_timer = self._node.create_timer(1, self.check_playing_callback)
 
         self._pulse = Pulse()
-        # Get default sink at startup
-        self._sink = self._pulse.get_sink_by_name(self._pulse.server_info().default_sink_name)
         self._get_volume_pub = self._node.create_publisher(Float32, "~/get/volume", 1)
         self._node.create_timer(1.0, self.publish_volume_callback)
         self._node.create_service(SetVolume, "~/set/volume", self.set_volume)
@@ -115,10 +113,12 @@ class AnnounceControllerProperty:
             self._prev_depart_and_arrive_type = message
 
     def publish_volume_callback(self):
+        self._sink = self._pulse.get_sink_by_name(self._pulse.server_info().default_sink_name)
         self._get_volume_pub.publish(Float32(data=self._sink.volume.value_flat))
 
     def set_volume(self, request, response):
         try:
+            self._sink = self._pulse.get_sink_by_name(self._pulse.server_info().default_sink_name)
             self._pulse.volume_set_all_chans(self._sink, request.volume)
             response.status.code = ResponseStatus.SUCCESS
         except Exception:
