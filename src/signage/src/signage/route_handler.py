3eadd4bcceff25ccf60e81a2eec585aec99707f3
Kah Hooi Tan <41041286+tkhmy@users.noreply.github.com>
2023-10-23 12:52:37 +0900

    do not announce emergency when stop mode! (#81)
    * do not announce emergency when stop mode!
    
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>
    
    * fix
    
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>
    
    * fix
    
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>
    
    ---------
    
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- src/signage/src/signage/route_handler.py
+++ src/signage/src/signage/route_handler.py
@@ -73,6 +73,8 @@ class RouteHandler:
     def emergency_checker_callback(self):
         if self._parameter.ignore_emergency:
             in_emergency = False
+        if self._autoware.information.operation_mode == OperationModeState.STOP:
+            in_emergency = False
         else:
             in_emergency = self._autoware.information.mrm_behavior == MrmState.EMERGENCY_STOP
 
